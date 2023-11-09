53a017277d9705cc706f942e1051105523a6bb2d
muslabz <ajetumobimust3@gmail.com>
2023-11-09 04:08:06 -0800

    sdfg

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-const filmsNum = process.argv[2] + '/';
-const urlFilms = 'https://swapi-api.hbtn.io/api/films/';
 
-request(urlFilms + filmsNum, async function (err, res, body) {
-
-    if (err) return console.error(err);
-
-    const urlList = JSON.parse(body).characters;
-
-    for (const url of urlList) {
-        await new Promise(function (resolve, reject) {
-            request(url, function (err, res, body) {
-                if (err) return console.error(err);
-                console.log(JSON.parse(body).name);
-                resolve();
-            });
-        });
-    }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
