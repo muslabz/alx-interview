13805e99c684067cc2b5ecc54dac519f5348554b
muslabz <ajetumobimust3@gmail.com>
2023-11-09 03:49:08 -0800

    sdfg

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,18 +1,24 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json';
-request.get(url, function (response, body) {
-  const film = (JSON.parse(body.body));
-  // debugging
-  // console.log(film);
-  // console.log(film.characters);
-  character(film.characters);
-});
-const character = (people, i = 0) => {
-  if (i === people.length) return;
-  request.get(people[i], function (response, body) {
-    // console.log(people[i]);
-    console.log(JSON.parse(body.body).name);
-    character(people, i + 1);
+const ID = process.argv[2];
+const URL_API = `https://swapi-api.hbtn.io/api/films/${ID}`;
+
+const getDataFromApi = async (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, function (error, response, body) {
+      if (error) reject(error);
+      else resolve(JSON.parse(body));
+    });
   });
 };
+
+const displayData = async (url) => {
+  const { characters } = await getDataFromApi(url);
+  for (const character of characters) {
+    const { name } = await getDataFromApi(character);
+    console.log(name);
+  }
+};
+
+displayData(URL_API);
